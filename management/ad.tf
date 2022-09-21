9aa279abc864bfde1fdccf144d6883e0958eac59
blurrrb <17aakashsingh1999@gmail.com>
2022-09-21 15:33:03 +0530

    remove management folder

--- management/ad.tf
+++
@@ -1,4 +0,0 @@
-data "oci_identity_availability_domain" "default_ad" {
-  compartment_id = var.tenancy_id
-  ad_number      = var.ad_number
-}
--- management/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_id
-  user_ocid        = var.user_id
-  private_key_path = var.private_key
-  private_key      = var.private_key
-  fingerprint      = var.fingerprint
-  region           = var.region
-}
